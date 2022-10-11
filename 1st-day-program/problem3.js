167aef1ab946e1a6d703fbe44c7509c941a5f60b
Sagar khadse <sagarkhadse6178@gmail.com>
2022-10-11 19:43:21 +0530

    variableIW

---
+++ 1st-day-program/problem3.js
@@ -0,0 +1,9 @@
+//Problem 3
+//Create a variable name to store your name
+//Create a variable age to store your age
+//Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let x = "Sagar";
+let y = 27;
+console.log(x,y);
+console.log(typeof(x),typeof(y));
---
+++ 1st-day-program/problem4.js
@@ -0,0 +1,19 @@
+//Problem 4
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+
+console.log("********************ReportCard***********************");
+let name="Sagar";
+let school="Adarsh Vidya Mandir";
+let grade="A";
+let section="B";
+let marks=250;
+console.log("Name:",name);
+console.log("School Name:",school);
+console.log("Grade:",grade);
+console.log("Section:",section);
+console.log("Marks in 3 Subject:",marks);
+console.log();
+console.log();
+console.log();
+console.log();
+console.log("*****************************************************");
